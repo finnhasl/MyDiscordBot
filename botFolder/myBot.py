1a3d361787ce8ba6cf43bb4ee956be4dbad96937
finnhasl <finn@georich.co.uk>
2025-11-17 18:05:34 +0000

    add on_message override functionality

--- botFolder/myBot.py
+++ botFolder/myBot.py
@@ -6,3 +6,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents =intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    print(message.content   )
+
+client.run(TOKEN)
